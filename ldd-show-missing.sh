d995850f68feb330ff6be1abb401a14b0eb597be
Fabio Durigon <develop@dury.it>
2022-03-28 17:42:51 +0200

    lun 28 mar 2022 17:42:50

---
+++ ldd-show-missing.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if [ -z "$1" ]; then
+	echo "Missing filename"
+	echo "Usage: $0 filename [output filename]"
+	exit
+fi
+readarray -t array <<< $(ldd $1 | grep "not found")
+
+for (( n=0; n < ${#array[*]}; n++)); do
+	var=${array[n]%" => "*}
+	var="${var#"${var%%[![:space:]]*}"}"
+	echo -e "\e[33mMissing $var\e[0m"
+	if [ -z "$2" ]; then
+		echo "$var" >> $2
+	fi
+done
