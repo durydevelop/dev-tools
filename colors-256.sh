f332f21350307205ca0ba28108497043e65aafb2
Fabio <develop@dury.it>
2022-03-31 15:37:34 +0200

    merge

--- colors-256.sh
+++ colors-256.sh
@@ -9,9 +9,11 @@
 for fgbg in 38 48 ; do # Foreground / Background
     for color in {0..255} ; do # Colors
         # Display the color
-        printf "\e[${fgbg};5;%sm  %3s  \e[0m" $color $color
-        # Display 6 colors per lines
-        if [ $((($color + 1) % 6)) == 4 ] ; then
+		fmt="e[${fgbg};5;""$color""m"
+        printf "%-11s" $fmt
+		printf "\e[${fgbg};5;%sm  %3s  \e[0m " $color $color
+        # Display 4 colors per lines
+        if [ $((($color + 1) % 4)) == 2 ] ; then
             echo # New line
         fi
     done
