822e4ba289cdb0f359a26f8e9720a0f662019496
fabio <develop@dury.it>
2023-08-31 21:55:26 +0200

    Thu Aug 31 21:55:26 CEST 2023

--- install-goldlake.sh
+++ install-goldlake.sh
@@ -33,39 +33,68 @@ function clone_if_not_exists() {
     fi
 }
 
-# Install packet if not exists
-# $1 packet name
+# Install pkg if does not exists
+# $1	->  pkg name (e.g. smb)
+# [$2]	->  command used to check pkg (e.g. smbpasswd)
 function install_if_not_exists() {
-    echo -n -e "Checking for \e[33m$1\e[0m command"
-    if ! command -v git &> /dev/null; then
-	echo ""
-	echo -e "\e[33m$1 is not installed, wait for installing...\e[0m"
-	sudo apt-get install -y $1;
-	if [ $? -eq 0 ]; then
-	    echo -e "\e[32m$1 install done\e[0m"
+	local MISSING=0
+	if [[ -z $2 ]]; then
+		# 2nd argument not found use dpkg
+		# search for "$1 " or "$1:" (for lib like libboost-dev:amd64)
+		RET=$(dpkg -l | grep "$1 \|$1:")
+		#echo "RET=$RET"
+		if [[ $RET == "" ]];then
+		# pkg not found
+		MISSING=1
+	    fi
 	else
-	    echo -e "\e[1;41m$1 install failed\e[0m"
-	    exit 1
+	    if ! command -v $2 &> /dev/null; then
+		# command not found
+		MISSING=1
+	    fi
 	fi
 	
-	# Check for missing dependencies
-	sudo apt-get install -y -f
-	if [ $? -eq 0 ]; then
-	    echo -e "\e[32mMissed dependency install complete\e[0m"
-	else
-	    echo -e "\e[1;41mMissed dependency install failed\e[0m"
-	exit 1
+	if [[ $MISSING == 1 ]]; then	
+		echo ""
+		echo -e -n "\e[33m$1 is not installed, install it? \e[0m"
+		read -p "(Y/n)" -n 1 -r
+			echo
+			if [[ $REPLY =~ ^[Nn]$ ]]; then
+				return
+			fi
+		sudo apt-get install -y $1;
+		if [ $? -eq 0 ]; then
+			echo -e "\e[32m$1 install done\e[0m"
+		else
+			echo -e "\e[1;41m$1 install failed\e[0m"
+		exit 1
+		fi
+
+		sudo apt-get install -y -f
+		if [ ! $? -eq 0 ]; then
+			echo -e "\e[1;41mMissed dependency install failed\e[0m"
+		fi
 	fi
+}
+
+# Write text line in a file: if does not exist, add it
+# $1    ->  filename
+# $2    ->  string to add as line
+function write_line_in_file_if_not_exists() {
+    if [[ -f $1 ]]; then
+        ret=$(grep -xF "$2" $1 || echo "$2" >> $1)
     else
-	echo -e "\e[32m OK\e[0m"
+        echo -e "\e[31m $1 NOT FOUND\e[0m"
     fi
 }
 
 #################################### entry-point ####################################
+EMSDK_ROOT=$DDEV_ROOT/cpp/lib/emsdk
 
 create_if_not_exists $DDEV_ROOT/cpp/src-mcu/rpi/goldlake
 clone_if_not_exists $DDEV_ROOT/cpp/src-mcu/rpi/goldlake/goldlake-frontend git@gitlab.com:durydevelop/cpp/src-mcu/rpi/goldlake/goldlake-frontend.git
-if [[ ! -d $DDEV_ROOT/cpp/lib/emsdk ]]; then
+
+if [[ ! -d $EMSDK_ROOT ]]; then
 	echo -n "emsdk is not installed, install it? "
 	read -p "(Y/n)" -n 1 -r
 	echo
@@ -73,7 +102,15 @@ if [[ ! -d $DDEV_ROOT/cpp/lib/emsdk ]]; then
 		exit 1
 	fi
 	install_if_not_exists bzip2
-	clone_if_not_exists $DDEV_ROOT/cpp/lib/emsdk https://github.com/emscripten-core/emsdk.git
-	cd $DDEV_ROOT/cpp/lib/emsdk
-	./emsdk install latest
+	clone_if_not_exists $EMSDK_ROOT https://github.com/emscripten-core/emsdk.git
+fi
+
+echo -e "\e[33mUpdate and activate emsdk\e[0m"
+EMSDK_QUIET=1
+cd $EMSDK_ROOT
+./emsdk install latest
+./emsdk activate latest --permanent &> /dev/null
+write_line_in_file_if_not_exists "$HOME/.profile" "EMSDK_QUIET=1 . $EMSDK_ROOT/emsdk_env.sh"
+if [[ $(printenv EMSDK) == "" ]]; then
+	echo -e "\e[38;5;9mYou need to restart your shell. Close and reopen it or type \"source ~/.profile\" command.\e[0m"
 fi
