0084ff98b42b1b929d8b23cd520b3028c3b36a8f
durydevelop <develop@dury.it>
2024-11-01 18:40:14 +0100

    Modifictao install-ddev.sh

--- install-ddev.sh
+++ install-ddev.sh
@@ -2,8 +2,7 @@
 
 # TODO: Check environments using array
 
-Version=1.0.8
-GITLAB_ACCESS_TOKEN="read-only:XRQgs6iGq8TQ6xvoDmDk"
+Version=2.0.0
 ENV_DDEV_GSOAP_TEMPLATES="DDEV_GSOAP_TEMPLATES"
 ENV_DDEV_ROOT_PATH="DDEV_ROOT"
 ENV_DDEV_TOOLS_PATH="DDEV_TOOLS"
@@ -11,20 +10,20 @@ ENV_DDEV_TOOLS_PATH="DDEV_TOOLS"
 # Result structure:
 # Dev\
 #     |cpp\
-#     |    |helpers_cmake\  <git@gitlab.com:durydevelop/cpp/helpers_cmake.git>
+#     |    |helpers_cmake\  <https://github.com/durydevelop/helpers_cmake.git>
 #     |    |
 #     |    |lib\
 #     |    |    	 |dpplib <git@gitlab.com:durydevelop/cpp/lib/dpptools.git>
+#     |    |         |Qt-ads <https://github.com/githubuser0xFFFF/Qt-Advanced-Docking-System.git>
 #     |    |
 #     |    |lib-mcu\
-#     |    |         |arduino-lib-oled
 #     |    |         |dpplib-mcu
 #     |    |
 #     |    |src\
 #     |    |
 #     |    |src-mcu\
 #     |    
-#     |dev-tools <git@gitlab.com:durydevelop/dev-tools.git>
+#     |dev-tools <https://github.com/durydevelop/dev-tools.git>
 
 print-usage() {
     echo "This script will install Dury Develop Framework."
@@ -34,6 +33,103 @@ print-usage() {
     echo -e "-h, --help\t\tPrint this help."
 }
 
+################################### Functions ####################################
+# Install pkg if does not exists
+# $1	->  pkg name (e.g. smb)
+# [$2]	->  command used to check pkg (e.g. smbpasswd)
+# return 1 on success otherwise 0
+# How to check return value:
+#if [[ $() == 0 ]]; then
+#	echo ""
+#	echo -e -n "\e[1;41m$1 install failed\e[0m"
+#fi
+function install_if_not_exists() {
+	local MISSING=0
+	if [[ -z $2 ]]; then
+		# 2nd argument not found use dpkg
+		# search for "$1 " or "$1:" (for lib like libboost-dev:amd64)
+		RET=$(dpkg -l | grep "$1 \|$1:")
+		#echo "RET=$RET"
+		if [[ $RET == "" ]];then
+		# pkg not found
+		MISSING=1
+	    fi
+	else
+	    if ! command -v $2 &> /dev/null; then
+		# command not found
+		MISSING=1
+	    fi
+	fi
+	
+	if [[ $MISSING == 1 ]]; then
+		echo ""
+		echo -e -n "\e[33m$1 is not installed, install it? \e[0m"
+		read -p "(Y/n)" -n 1 -r
+		echo
+		if [[ $REPLY =~ ^[Nn]$ ]]; then
+			return 1
+		fi
+		sudo apt-get install -y $1;
+		if [ $? -eq 0 ]; then
+			echo -e "\e[32m$1 install done\e[0m"
+		else
+			return 0
+		fi
+
+		sudo apt-get install -y -f
+		if [ ! $? -eq 0 ]; then
+			echo -e "\e[1;41mMissed dependency install failed\e[0m"
+			return 0
+		fi
+		return 1
+	fi
+}
+
+# Create $1 folder if does not exists
+function dir_create_if_not_exists() {
+    echo -n -e "Checking for \e[33m$1\e[0m folder "
+    if [[ ! -d "$1" ]]; then
+        mkdir -p $1
+        if [[ ! -d "$1" ]]; then
+            echo -e "\e[31mCan not create...stopping\e[0m"
+            exit
+        fi
+        echo -e "\e[32mCREATED\e[0m"
+    else
+        echo -e "\e[32mOK\e[0m"
+    fi
+}
+
+# Clone a git repo using $1 as dest folder and $2 as git url
+function git_clone_if_not_exists() {
+    echo -n -e "Check for \e[33m$1\e[0m repo "
+    if [[ ! -d "$1/.git" ]]; then
+        echo ""
+        echo -e "\e[33mCloning $2\e[0m"
+        git clone $2 $1
+        ret=$?
+        if [[ $ret -eq 0 ]]; then
+            echo -e "Cloned \e[32mOK\e[0m"
+        else
+            echo -e "\e[1;41mERROR $ret\e[0m"
+            error_msg="Cloning $2 failed.\nMay be you don't have a ssh public key, try to generate it with command 'ssh-keygen -t rsa -b4096 -C \"your@mail.it\"' or try to use -https option".
+        fi
+    else
+        echo -e "\e[32mOK\e[0m"
+    fi
+}
+
+# Write text line in a file: if does not exist, add it
+# $1    ->  filename
+# $2    ->  string to add as line
+function write_line_in_file_if_not_exists() {
+    if [[ -f $1 ]]; then
+        ret=$(grep -xF "$2" $1 || echo "$2" >> $1)
+    else
+        echo -e "\e[31m $1 NOT FOUND\e[0m"
+    fi
+}
+
 #################################### entry-point ####################################
 
 # Full path of the current script
@@ -41,9 +137,6 @@ THIS=`readlink -f "${BASH_SOURCE[0]}" 2>/dev/null||echo $0`
 
 # The directory where current script resides
 DIR=`dirname "${THIS}"`
-
-# Include script library ('Dot' means 'source', i.e. 'include':)
-. "$DIR/lib.sh"
 
 # Parse command line
 POSITIONAL=()
@@ -77,6 +170,7 @@ install_if_not_exists cmake
 install_if_not_exists build-essential
 install_if_not_exists libboost-dev
 install_if_not_exists libopencv-dev
+install_if_not_exists libgl1-mesa-dev
 
 ## Qt dependences
 # possible:
@@ -191,24 +285,20 @@ dir_create_if_not_exists "$DDEV_ROOT_PATH/cpp/lib-mcu"
 # cpp/src-mcu
 dir_create_if_not_exists "$DDEV_ROOT_PATH/cpp/src-mcu"
 
-## Clone repo
+## Clone repositories
 echo "Clone repositories"
 
 # Clone dev-tools
-git_clone_if_not_exists $DDEV_TOOLS_PATH git@gitlab.com:durydevelop/dev-tools.git
+git_clone_if_not_exists $DDEV_TOOLS_PATH https://github.com/durydevelop/dev-tools.git
 
 # Clone helpers_cmake
-git_clone_if_not_exists "$DDEV_ROOT_PATH/cpp/helpers_cmake" git@gitlab.com:durydevelop/cpp/helpers_cmake.git
+git_clone_if_not_exists "$DDEV_ROOT_PATH/cpp/helpers_cmake" https://github.com/durydevelop/helpers_cmake.git
 
 # Clone dpplib
-git_clone_if_not_exists "$DDEV_ROOT_PATH/cpp/lib/dpplib" git@gitlab.com:durydevelop/cpp/lib/dpptools.git
+git_clone_if_not_exists "$DDEV_ROOT_PATH/cpp/lib/dpplib" https://github.com/durydevelop/dpplib.git
 
-# Clone mcu libs
-git_clone_if_not_exists "$DDEV_ROOT_PATH/cpp/lib-mcu/ddigitalio" git@gitlab.com:durydevelop/cpp/lib/mcu/ddigitalio.git
-git_clone_if_not_exists "$DDEV_ROOT_PATH/cpp/lib-mcu/dmcomm" git@gitlab.com:durydevelop/cpp/lib/mcu/dmcomm.git
-git_clone_if_not_exists "$DDEV_ROOT_PATH/cpp/lib-mcu/dservo" git@gitlab.com:durydevelop/cpp/lib/mcu/dservo.git
-git_clone_if_not_exists "$DDEV_ROOT_PATH/cpp/lib-mcu/dmenu" git@gitlab.com:durydevelop/cpp/lib/mcu/dmenu.git
-git_clone_if_not_exists "$DDEV_ROOT_PATH/cpp/lib-mcu/ddcmotorwheels" git@gitlab.com:durydevelop/cpp/lib/mcu/ddcmotorwheels.git
+# Clone dpplibmcu
+git_clone_if_not_exists "$DDEV_ROOT_PATH/cpp/lib-mcu/dpplibmcu" https://github.com/durydevelop/dpplibmcu.git
 
 
 # Clone Qt Advanced Docking
